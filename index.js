0c1f9cd3b5cae221ab3d6a72e6fbdaba70088925
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-12-12 11:27:21 +0530

    check objects are same or equal their properties

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-address = {
-    street:"2nd Lane",
-    city:"Embilipitiya",
-    zipCode:"70200",
-}
-
-// factory function
-function createAddress(street,city,zipCode){
-    return{
-        street,
-        city,
-        zipCode
-    }
-}
+let address1 = new Address('a', 'b', 'c')
+let address2 = new Address('a', 'b', 'c')
 
 // constructor fuction
-function Address(street,city,zipCode){
-    this.street=street,
-    this.city=city,
-    this.zipCode=zipCode
+function Address(street, city, zipCode) {
+    this.street = street,
+        this.city = city,
+        this.zipCode = zipCode
 }
 
-console.log(new Address("A","B","689"))
+//check objects properties are same
+function areEqual(address1, address2) {
+    for (let key in address1) {
+        if (address1[key] !== address2[key]) {
+            return false
+        }
+    }
+    return true
+}
+
+// check objects reference are same
+function areSame(address1, address2) {
+    return address1 === address2 ? true : false
+}
+
+console.log(areEqual(address1, address2))
